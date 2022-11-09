6a43a95c5a58240cdbb87d6c134ecc167fbc3dd2
Ondřej Rygl <xryglo00@vutbr.cz>
2022-11-09 01:28:27 +0100

    login screen, thingsboard app connection.

--- xryglo00/api/views.py
+++ xryglo00/api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics
 # Create your views here.
 
 
 def main(requst):
     return HttpResponse("<h1>Stránka pro API, TBD<h1>")
+
+class CreteLoginView(APIView):
+    serializer_class = LoginSerializer
+    def post(self, request, format=None):
+        pass
