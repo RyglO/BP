f602df8d4dd9b33f7a5d09d0379eb130df4ab429
Ondřej Rygl <xryglo00@vutbr.cz>
2022-11-15 20:35:01 +0100

    frontent lib changes

--- xryglo00/frontend/src/components/LoginPage.js
+++ xryglo00/frontend/src/components/LoginPage.js
@@ -5,10 +5,6 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
-import Link from 'react-router-dom';
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
 
 
 export default class LoginPage extends Component {
