4e32adb3bfa1def6cfcfbeb11b01054ce3414067
Ondřej Rygl <xryglo00@vutbr.cz>
2023-04-11 16:44:51 +0200

    pictures on modules

--- xryglo00/frontend/src/components/ModulesList.js
+++ xryglo00/frontend/src/components/ModulesList.js
@@ -1,8 +1,7 @@
-import { Card, Grid, Typography } from "@mui/material";
+import { Card, CardMedia, Grid, Typography, CardActionArea } from "@mui/material";
 import React from "react";
 import modules from "../../static/modules.json"
-import { Link } from "react-router-dom"; 
-
+import { Link } from "react-router-dom"
 
 const ModuleList = () => {
     
@@ -10,14 +9,18 @@ const ModuleList = () => {
     return (
         <Grid container spacing={4}>
             {modules.modules.map((module) => (
-                <Grid item key={module} xs={12} md={3} >
-                    <Card variant="outlined" align='center' >                            
-                        <Link to={`${module.link}`}>
-                            <Typography variant="subtitle1" align="center" color="text.secondary" paragraph>
-                                {module.name}
-                            </Typography>
-                        </Link>
-                    </Card>   
+                <Grid item key={module} xs={8} md={2}>
+                    <CardActionArea>
+                        <Card variant="outlined" align='center'>              
+                            <Link to={`${module.link}`}>
+                                <CardMedia component="img" image={`../../static/images/${module.picture}`} />
+                                <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                                    {module.name}
+                                </Typography>
+                            </Link>
+                        </Card>       
+                    </CardActionArea>
+                    
                 </Grid>
 
             ))}
